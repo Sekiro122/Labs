cae7141543d5de77b9eec823c36f7896cb6401c3
Lynx <148974194+Sekiro122@users.noreply.github.com>
2026-03-17 00:56:05 -0500

    Add files via upload

---
+++ лаба 7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int myNumber = 777;
+                double myPrice = 99.99;
+                char myLetter = 'X';
+
+                int* pointerToNumber = &myNumber;
+                double* pointerToPrice = &myPrice;
+                char* pointerToLetter = &myLetter;
+
+                Console.WriteLine($"myNumber  = {myNumber}  (адрес: {(long)pointerToNumber:X})");
+                Console.WriteLine($"myPrice   = {myPrice} (адрес: {(long)pointerToPrice:X})");
+                Console.WriteLine($"myLetter  = {myLetter}   (адрес: {(long)pointerToLetter:X})");
+
+                *pointerToNumber = 111;        
+                *pointerToPrice = 49.50;        
+                *pointerToLetter = 'Z';          
+
+                Console.WriteLine($"myNumber  = {myNumber}  (адрес: {(long)pointerToNumber:X})");
+                Console.WriteLine($"myPrice   = {myPrice} (адрес: {(long)pointerToPrice:X})");
+                Console.WriteLine($"myLetter  = {myLetter}   (адрес: {(long)pointerToLetter:X})");
+            }
+        }
+    }
+}
